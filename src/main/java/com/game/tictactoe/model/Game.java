29a886139113576e89dd526240bc3e8c52ddfb00
2025-DEV1-011 <gowthamraj07@gmail.com>
2025-10-06 10:33:00 +0200

    Make move: Should switch current player after successful move

--- src/main/java/com/game/tictactoe/model/Game.java
+++ src/main/java/com/game/tictactoe/model/Game.java
@@ -5,12 +5,18 @@ import java.util.UUID;
 public class Game {
     private final String gameId;
     private final Player[][] board;
-    private final Player currentPlayer;
+    private Player currentPlayer;
 
     public Game() {
         this.gameId = UUID.randomUUID().toString();
         this.board = new Player[3][3];
         this.currentPlayer = Player.X;
+    }
+
+    Game(Player player) {
+        this.gameId = UUID.randomUUID().toString();
+        this.board = new Player[3][3];
+        this.currentPlayer = player;
     }
 
     public String getGameId() {
@@ -23,6 +29,10 @@ public class Game {
 
     public void makeMove(int row, int col) {
         board[row][col] = currentPlayer;
+
+        // Switch player after move
+        currentPlayer = (currentPlayer == Player.X) ? Player.O : Player.X;
+
     }
 
     public Player getCurrentPlayer() {
--- src/test/java/com/game/tictactoe/model/GameTest.java
+++ src/test/java/com/game/tictactoe/model/GameTest.java
@@ -2,6 +2,8 @@ package com.game.tictactoe.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
@@ -16,5 +18,26 @@ class GameTest {
 
         // Assert
         assertEquals(Player.X, game.getBoard()[row][col], "Cell should be updated to Player.X after move");
+    }
+
+
+    @Test
+    void makeMove_shouldSwitchCurrentPlayerAfterSuccessfulMove() {
+        // Arrange
+        Player randomPlayer = getRandomPlayer();
+        Game game = new Game(randomPlayer);
+
+        // Act
+        int row = 0, col = 0;
+        game.makeMove(row, col);
+
+        // Assert
+        Player expectedPlayer = (randomPlayer == Player.X) ? Player.O : Player.X;
+        assertEquals(expectedPlayer, game.getCurrentPlayer(), "Player should be O after First move");
+    }
+
+    private static Player getRandomPlayer() {
+        Player[] players = Player.values();
+        return players[new Random().nextInt(players.length)];
     }
 }
